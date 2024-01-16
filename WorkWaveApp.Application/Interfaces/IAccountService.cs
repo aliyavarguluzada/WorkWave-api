fa6af39cad882dcc83c643166633d24a4e5add13
Aliyavar <aliyavarguluzada@gmail.com>
2024-01-16 06:38:47 -0800

    Created IAccountService interface and added Register and Login method should create the concrete of this and implement the interface more method may be added later

---
+++ WorkWaveApp.Application/Interfaces/IAccountService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WorkWaveApp.Models.v1.Account.Login;
+using WorkWaveApp.Models.v1.Account.Register;
+using WorkWaveAPP.Application.Core;
+
+namespace WorkWaveApp.Application.Interfaces
+{
+    public interface IAccountService
+    {
+        Task<ServiceResult<RegisterResponse>> Register(RegisterRequest request);
+        Task<ServiceResult<LoginResponse>> Login(LoginRequest request);
+    }
+}
