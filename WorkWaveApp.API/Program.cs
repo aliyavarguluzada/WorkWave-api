ad81434ccf872e0fb1066b2d2a2cc51a500e18d6
Aliyavar <aliyavarguluzada@gmail.com>
2024-01-26 05:20:20 -0800

    used configuration to add logging settings and added try catch incase of any exception

--- WorkWaveApp.API/Program.cs
+++ WorkWaveApp.API/Program.cs
@@ -61,33 +61,51 @@ builder.Services.AddAuthorization();
 
 builder.Services.AddCors();
 
-Log.Logger = new LoggerConfiguration()
-    .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
-    .Enrich.FromLogContext()
-    .WriteTo.Console()
-    .WriteTo.Debug(Serilog.Events.LogEventLevel.Information)
-    .WriteTo.File("wwwroot/logs/appLog-.txt", rollingInterval: RollingInterval.Day)
-    .CreateBootstrapLogger();
 
+var configuration = new ConfigurationBuilder()
+       .SetBasePath(Directory.GetCurrentDirectory())
+       .AddJsonFile("appsettings.json")
+       .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json", true)
+       .Build();
 
-builder.Host.UseSerilog();
+var logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(configuration)
+    .CreateLogger();
 
-var app = builder.Build();
-
-app.UseSerilogRequestLogging();
-
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+try
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    Log.Information("Starting Web Application");
+    builder.Host.UseSerilog((context, services, configuration) => configuration
+        .ReadFrom.Configuration(context.Configuration)
+        .ReadFrom.Services(services)
+        .Enrich.FromLogContext()
+        .WriteTo.Console());
+    var app = builder.Build();
+
+    app.UseSerilogRequestLogging();
+
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.Run();
+
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Application terminated unexpectedly");
+}
+finally
+{
+    Log.CloseAndFlushAsync();
+}
