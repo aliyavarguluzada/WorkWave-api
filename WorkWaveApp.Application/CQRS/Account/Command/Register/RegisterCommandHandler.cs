b34354b74270a8cd0ed635fedbf5bf41176cbb91
Aliyavar <aliyavarguluzada@gmail.com>
2024-02-17 08:22:00 -0800

    should use a client side app to test a few things like refresh token. gonna implement authorizarization next

--- WorkWaveApp.Application/CQRS/Account/Command/Register/RegisterCommandHandler.cs
+++ WorkWaveApp.Application/CQRS/Account/Command/Register/RegisterCommandHandler.cs
@@ -1,14 +1,10 @@
 ﻿using FluentValidation;
 using MediatR;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using WorkWaveApp.Application.Interfaces;
+using WorkWaveApp.Domain.Enums;
 using WorkWaveApp.Models.v1.Account.Register;
 using WorkWaveAPP.Application.Core;
-using WorkWaveApp.Domain.Enums;
+
 namespace WorkWaveApp.Application.CQRS.Account.Command.Register
 {
     public class RegisterCommandHandler : IRequestHandler<RegisterCommand, ServiceResult<RegisterResponse>>
@@ -26,15 +22,16 @@ namespace WorkWaveApp.Application.CQRS.Account.Command.Register
         {
             var validationResult = await _validator.ValidateAsync(request);
 
-            bool validation = validationResult.IsValid;
 
             if (!validationResult.IsValid)
             {
                 var errors = validationResult.Errors
-                      .GroupBy(x => x.PropertyName)
-                      .ToDictionary(
-                      g => g.Key,
-                      g => g.Select(x => x.ErrorMessage).ToArray());
+                                             .GroupBy(x => x.PropertyName)
+                                             .ToDictionary(
+                                             g => g.Key,
+                                             g => g.Select(x => x.ErrorMessage)
+                                             .ToArray());
+                    
                 foreach(var item in errors)
                 {
                     Console.WriteLine(item);
